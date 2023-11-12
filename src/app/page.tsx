e356fe4be07f8d3686057b01cea349099e015cc4
fahimshahriyer <shahriyers@gmail.com>
2023-11-13 01:38:17 +0600

    responsive issue fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="min-h-screen p-8 md:p-8 lg:p-16 container mx-auto">
       <div className="hero-section mt-16">
-        <p className="greetings uppercase text-center font-bold text-[120px] md:text-[160px] lg:text-[220px] xl:text-[320px]">
+        <p className="greetings uppercase text-center font-bold text-[80px] md:text-[160px] lg:text-[220px] xl:text-[320px]">
           Hello
         </p>
       </div>
@@ -12,11 +12,11 @@ export default function Home() {
         <p className="uppercase m-2 text-sm md:text-xl lg:text-2xl xl:text-3xl">
           I am
         </p>
-        <p className="uppercase text-2xl md:text-4xl lg:text-5xl xl:text-6xl text-sky-400 font-semibold">
+        <p className="uppercase text-xl md:text-4xl lg:text-5xl xl:text-6xl text-sky-400 font-semibold">
           Fahim Shahriyer
         </p>
       </div>
-      <div className="mt-40 xl:mt-56 grid gap-4 grid-cols-1 md:grid-cols-3 pr-4 md:pr-24 pl-4 md:pl-24">
+      <div className="mt-24 xl:mt-56 grid gap-4 grid-cols-1 md:grid-cols-3 pr-4 md:pr-24 pl-4 md:pl-24">
         <div className="col-span-3 md:col-span-2">
           <p className="uppercase text-center md:text-right text-sky-400">
             Software Development Leadership
@@ -28,13 +28,13 @@ export default function Home() {
           </p>
         </div>
       </div>
-      <div className="mt-8 grid gap-4 grid-cols-1 md:grid-cols-3 pr-4 md:pr-24 pl-4 md:pl-24">
+      <div className="mt-8 grid gap-4 grid-cols-1 md:grid-cols-3 md:pr-24 pl-4 md:pl-24">
         <div className="col-span-3 md:col-span-2">
           <p className="text-center md:text-right text-sky-500 text-sm">
             contact
           </p>
         </div>
-        <div className="col-span-3 md:col-span-1 mt-4 md:mt-0">
+        <div className="col-span-3 md:col-span-1 md:mt-0">
           <p className="text-center md:text-left text-sm mb-2">
             <a href="mailto:shahriyers@gmail.com">shahriyers@gmail.com</a>
           </p>
